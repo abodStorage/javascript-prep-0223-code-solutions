7b4dec88afad2a90d9d2a49b978c2642e09101d6
aBoDgItHuB <hoonnam.kim@gmail.com>
2023-03-08 04:22:52 +0000

    [VERBOSE] Completed - prep-javascript-arrays

---
+++ 0070    prep-javascript-arrays/main.js
@@ -0,0 +1,37 @@
+// Declare arrays - languages
+const languages =
+[
+  'html',
+  'css',
+  'javascript',
+  'react',
+  'node',
+  'postgres'
+];
+
+// Print out
+console.log(languages);
+
+// Declare variable - firstElement
+const firstElement = languages[0];
+
+// Print out
+console.log('The first element of the array is ' + firstElement + '.');
+
+// Declare variable - thirdElement
+const thirdElement = languages[2];
+
+// Print out
+console.log('The third element of the array is ' + thirdElement + '.');
+
+// Declare variable - length
+const length = languages.length;
+
+// Print out
+console.log('The length of the array is ' + length + '.');
+
+// Declare variable - lastElement
+const lastElement = languages[length - 1];
+
+// Print out
+console.log('The last element of the array is ' + lastElement + '.');
